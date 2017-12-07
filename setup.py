9c9d9bc71d6b4a49adfb4aa7b2ec11a73fc47676
petetetete <huettlpeter@gmail.com>
2017-12-07 16:55:33 -0700

    Update release information in setup.py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ def read(fname):
 
 
 setup(name='cross-doc',
-      version='0.0.2',
+      version='0.1.0',
       description='A comment management system for the modern world',
       long_description=read('README.rst'),
       author='Octo-Docs',
       author_email='ph289@nau.edu',
       license='MIT',
       url='https://github.com/petetetete/CrossDoc',
-      download_url='https://github.com/petetetete/CrossDoc/archive/0.0.2.tar.gz',
+      download_url='https://github.com/petetetete/CrossDoc/archive/0.1.0.tar.gz',
       keywords='CrossDoc documentation comments storage',
       packages=['cdoc'],
       entry_points={
