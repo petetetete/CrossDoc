a54b211d9623ed2489f07c80cbd90ec421738bc2
petetetete <huettlpeter@gmail.com>
2018-02-08 22:21:09 -0700

    Implement basic 'insert comment' in Sublime

--- te-plugins/sublime/CrossDoc.py
+++ te-plugins/sublime/CrossDoc.py
@@ -1,15 +1,29 @@
 import sublime
 import sublime_plugin
+from subprocess import check_output
 
 
 class InsertCommentCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         v = self.view
 
-        first_cursor = v.sel()[0]
-        curr_line = v.line(first_cursor)
+        # Get line info
+        line = v.line(v.sel()[0])
+        line_text = v.substr(line)
+        white_space = line_text[:len(line_text) - len(line_text.lstrip())]
 
-        # Toggle the comment for the
+        # Create and add comment to view
+        anchor = check_output("cdoc generate-anchor",
+                              shell=True).decode("utf-8").rstrip()
+        string = white_space + anchor + "\n"
+        v.insert(edit, line.begin(), string)
+
+        # Move cursor to the end of the new comment
+        v.sel().clear()
+        v.sel().add(sublime.Region(line.begin() + len(string) - 1))
+
+        # Toggle comment for the new string
         v.run_command("toggle_comment")
-        self.view.insert(edit, curr_line.a + 2,
-                         "<&> ANCHOR: Example comment \n")
+
+        print(check_output("cdoc generate-anchor", shell=True).rstrip())
