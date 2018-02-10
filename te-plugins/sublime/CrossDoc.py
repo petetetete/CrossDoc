3b6f18a12a23a6481b7812abee78f4e9c870efa8
petetetete <huettlpeter@gmail.com>
2018-02-10 14:27:13 -0700

    Implmement Sublime 'delete comment' functionality

--- te-plugins/sublime/CrossDoc.py
+++ te-plugins/sublime/CrossDoc.py
@@ -4,13 +4,16 @@ from subprocess import check_output
 import os
 
 
+ANCHOR_HOOK = "<&> "
+
+
 class InsertCommentCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         v = self.view
 
         # Get line info
-        line = v.line(v.sel()[0])
+        line = v.full_line(v.sel()[0])
         line_text = v.substr(line)
         white_space = line_text[:len(line_text) - len(line_text.lstrip())]
 
@@ -18,15 +21,16 @@ class InsertCommentCommand(sublime_plugin.TextCommand):
         cwd = os.path.dirname(v.file_name())
 
         # Create and add comment to view
-        anchor = check_output('cdoc create-comment -text "[COMMENT TEXT]"',
+        output = check_output("cdoc create-comment -text \"[COMMENT TEXT]\"",
                               shell=True, cwd=cwd).decode("utf-8").rstrip()
 
-        if anchor.startswith("fatal"):
-            print(anchor)
+        # Catch command line errors
+        if output.startswith("fatal"):
+            print(output)
             return
 
-        print(anchor)
-        string = white_space + anchor + "\n"
+        # Insert created comment to file
+        string = white_space + output + "\n"
         v.insert(edit, line.begin(), string)
 
         # Move cursor to the end of the new comment
@@ -35,3 +39,43 @@ class InsertCommentCommand(sublime_plugin.TextCommand):
 
         # Toggle comment for the new string
         v.run_command("toggle_comment")
+
+
+class DeleteCommentCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        v = self.view
+
+        # Get line info
+        line = v.full_line(v.sel()[0])
+        line_text = v.substr(line)
+
+        # Catch attempting to use the delete while not selecting a comment
+        if ANCHOR_HOOK not in line_text:
+            print("fatal: not selecting a comment")
+            return
+
+        # Start finding the anchor
+        split_line = line_text.split(" ")
+        anchor_index = split_line.index(ANCHOR_HOOK.strip()) + 1
+
+        # Catch invalid anchor errors
+        if anchor_index >= len(split_line):
+            print("fatal: cannot find anchor")
+            return
+
+        # Save the found anchor
+        anchor = split_line[anchor_index]
+
+        # Get current working directory and run delete
+        cwd = os.path.dirname(v.file_name())
+        output = check_output("cdoc delete-comment -a " + anchor,
+                              shell=True, cwd=cwd).decode("utf-8").rstrip()
+
+        # Catch command line errors
+        if output.startswith("fatal"):
+            print(anchor)
+            return
+
+        # Clear the line in the file
+        v.erase(edit, line)
