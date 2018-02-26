b6aadff600a09d3700bafbc0533fc2a256e92b20
Peter <huettlpeter@gmail.com>
2018-02-26 12:30:12 -0700

    Modify the Emacs plugin to support multi-line comment creation

--- te-plugins/emacs/CrossDoc.el
+++ te-plugins/emacs/CrossDoc.el
@@ -49,10 +49,6 @@
 (defun insert-comment ()
   (interactive)
 
-  ; Move to beginning of line and tab to match
-  (move-beginning-of-line nil)
-  (indent-according-to-mode)
-
   ; Get CrossDoc command line output from create-comment
   (setq output (substring (shell-command-to-string "cdoc cc -t \"[COMMENT HERE]\"") 0 -1))
   (if (not (string= "fatal" (substring output 0 5)))
@@ -60,13 +56,22 @@
     ; If not fatal: continue creating comment
     (progn
 
-      ; Insert CrossDoc comment (and comment it out)
-      (insert output)
-      (comment-region (line-beginning-position) (line-end-position))
-      
-      ; Move the cursor back to the line it started, and fix the tabbing
-      (insert "\n")
-      (indent-according-to-mode))
+      ; Move to beginning of line and tab to match
+      (move-beginning-of-line nil)
+      (indent-according-to-mode)
+
+      ; For each of the lines of the comment
+      (let ((output-split (split-string output "\n" t)))
+        (dolist (elt output-split)
+          (progn
+
+            ; Insert actual comment line (and comment)
+            (insert elt)
+            (comment-region (line-beginning-position) (line-end-position))
+
+            ; Correct the pointer position
+            (insert "\n")
+            (indent-according-to-mode)))))
 
     ; Else if fatal: forward the fatal message to the console
     (message output)))
