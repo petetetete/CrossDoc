c3d8ef10bd98b07ea8b9d8f9e33804742fe3ccae
Peter <huettlpeter@gmail.com>
2018-04-05 16:10:04 -0700

    Add next_set command and hotkeys to Sublime plugin

--- te-plugins/sublime/CrossDoc.py
+++ te-plugins/sublime/CrossDoc.py
@@ -31,8 +31,6 @@ class InsertCommentCommand(sublime_plugin.TextCommand):
 
     # Get line info
     line = v.full_line(v.sel()[0])
-    line_text = v.substr(line)
-    white_space = line_text[:len(line_text) - len(line_text.lstrip())]
 
     # Get current working directory
     cwd = os.path.dirname(v.file_name())
@@ -46,28 +44,8 @@ class InsertCommentCommand(sublime_plugin.TextCommand):
       print(output)
       return
 
-    # Split output line by carriage return
-    output_lines = [white_space + l + "\n" for l in output.split("\r\n")]
-
-    # Insert created comment to file
-    v.insert(edit, line.begin(), "".join(output_lines))
-
-    # Clear all cursors and save the line beginning
-    v.sel().clear()
-    char_point = line.begin()
-
-    # For each line added to the file, add a cursor
-    for output_line in output_lines:
-      v.sel().add(sublime.Region(char_point))
-      char_point += len(output_line)
-
-    # Toggle comment for the new string
-    v.run_command("toggle_comment")
-
-    # Clear all cursors and reset the cursor to the end of the new comment
-    last_cursor = v.sel()[-1]
-    v.sel().clear()
-    v.sel().add(sublime.Region(v.full_line(last_cursor).b - 1))
+    # Insert comment in to file
+    insertComment(edit, v, line, output)
 
 
 class DeleteCommentCommand(sublime_plugin.TextCommand):
@@ -75,36 +53,9 @@ class DeleteCommentCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     v = self.view
 
-    # Get line info
-    line = v.line(v.sel()[0])
-
-    # Catch attempting to use the delete while not selecting a comment
-    if not v.match_selector(line.end(), "comment"):
-      print("fatal: not selecting a comment")
-      return
-
-    # Find the CrossDoc hook region
-    h_region = line
-    while (v.match_selector(h_region.end(), "comment") and
-           ANCHOR_HOOK not in v.substr(h_region)):
-      h_region = v.line(h_region.begin() - 1)
-
-    # If we ran out of comment before finding an anchor
-    if ANCHOR_HOOK not in v.substr(h_region):
-      print("fatal: cannot find anchor")
-      return
-
-    # Save the line's text
-    h_line_text = v.substr(h_region)
-
-    # Try to get the anchor and set from the line
-    try:
-      anchor = getAnchorFromLine(h_line_text)
-      # TODO: set = getSetFromLine(h_line_text)
-
-    # Catch maniuplated hook lines
-    except ValueError:
-      print("fatal: cannot find anchor")
+    # Get comment info
+    h_region, anchor, set = getCommentInfo(edit, v)
+    if h_region is None:
       return
 
     # Get current working directory and run delete
@@ -117,14 +68,8 @@ class DeleteCommentCommand(sublime_plugin.TextCommand):
       print(output)
       return
 
-    # Find the start and end of the comment region
-    c_region_start = h_region.begin()
-    while v.match_selector(h_region.end(), "comment"):
-      h_region = v.line(h_region.end() + 1)
-    c_region_end = h_region.begin()
-
-    # Remove the comment from the actual file
-    v.erase(edit, sublime.Region(c_region_start, c_region_end))
+    # Delete comment
+    deleteComment(edit, v, h_region)
 
 
 class UpdateCommentsCommand(sublime_plugin.TextCommand):
@@ -180,6 +125,31 @@ class UpdateCommentsCommand(sublime_plugin.TextCommand):
         return
 
 
+class NextSetCommand(sublime_plugin.TextCommand):
+
+  def run(self, edit):
+    v = self.view
+
+    # Get comment info
+    h_region, anchor, set = getCommentInfo(edit, v)
+
+    # Get current working directory and run delete
+    cwd = os.path.dirname(v.file_name())
+    output = check_output("cdoc fc -a " + anchor + " -set \"" + set + "\" --ns",
+                          shell=True, cwd=cwd).decode("utf-8").rstrip()
+
+    # Catch command line errors
+    if output.startswith("fatal"):
+      print(output)
+      return
+
+    # Delete existing comment
+    deleteComment(edit, v, h_region)
+
+    # Insert comment in to file
+    insertComment(edit, v, v.line(v.sel()[0]), output)
+
+
 # Event listener callbacks
 
 class UpdateOnSave(sublime_plugin.EventListener):
@@ -189,6 +159,84 @@ class UpdateOnSave(sublime_plugin.EventListener):
 
 
 # Helper Methods #
+
+def getCommentInfo(edit, v):
+
+  # Get line info
+  line = v.line(v.sel()[0])
+
+  # Catch attempting to use the delete while not selecting a comment
+  if not v.match_selector(line.end(), "comment"):
+    print("fatal: not selecting a comment")
+    return None, None, None
+
+  # Find the CrossDoc hook region
+  h_region = line
+  while (v.match_selector(h_region.end(), "comment") and
+         ANCHOR_HOOK not in v.substr(h_region)):
+    h_region = v.line(h_region.begin() - 1)
+
+  # If we ran out of comment before finding an anchor
+  if ANCHOR_HOOK not in v.substr(h_region):
+    print("fatal: cannot find anchor")
+    return None, None, None
+
+  # Save the line's text
+  h_line_text = v.substr(h_region)
+
+  # Try to get the anchor and set from the line
+  try:
+    anchor = getAnchorFromLine(h_line_text)
+    set = getSetFromLine(h_line_text)
+
+  # Catch maniuplated hook lines
+  except ValueError:
+    print("fatal: cannot find anchor")
+    return None, None, None
+
+  return h_region, anchor, set
+
+
+def insertComment(edit, v, line, comment):
+
+  line_text = v.substr(line)
+  white_space = line_text[:len(line_text) - len(line_text.lstrip())]
+
+  # Split output line by carriage return
+  comment_lines = [white_space + l + "\n" for l in comment.split("\r\n")]
+
+  # Insert created comment to file
+  v.insert(edit, line.begin(), "".join(comment_lines))
+
+  # Clear all cursors and save the line beginning
+  v.sel().clear()
+  char_point = line.begin()
+
+  # For each line added to the file, add a cursor
+  for comment_line in comment_lines:
+    v.sel().add(sublime.Region(char_point))
+    char_point += len(comment_line)
+
+  # Toggle comment for the new string
+  v.run_command("toggle_comment")
+
+  # Clear all cursors and reset the cursor to the end of the new comment
+  last_cursor = v.sel()[-1]
+  v.sel().clear()
+  v.sel().add(sublime.Region(v.full_line(last_cursor).b - 1))
+
+
+def deleteComment(edit, v, line):
+
+  # Find the start and end of the comment region
+  c_region_start = line.begin()
+  while v.match_selector(line.end(), "comment"):
+    line = v.line(line.end() + 1)
+  c_region_end = line.begin()
+
+  # Remove the comment from the actual file
+  v.erase(edit, sublime.Region(c_region_start, c_region_end))
+
 
 def getAnchorFromLine(line):
   """Gets anchor from a line string
