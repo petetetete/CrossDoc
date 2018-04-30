4dc8576b83474adcf85ed98c384cb6dafd751713
Peter <huettlpeter@gmail.com>
2018-04-30 13:10:54 -0700

    Update setuptools version to 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='cross-doc',
-      version='0.1.3',
+      version='0.2.0',
       description='A comment management system for the modern world',
       long_description=read('README.rst'),
       author='Octo-Docs',
