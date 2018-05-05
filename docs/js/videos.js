80194c248b8cebd8f1cc2e8050582464eeef9cff
petetetete <huettlpeter@gmail.com>
2018-05-05 12:28:14 -0700

    Add direct video linking

--- docs/js/videos.js
+++ docs/js/videos.js
@@ -1,18 +1,29 @@
 function changeVideo() {
-    links.forEach(link => link.classList.remove("videos__link--selected"))
-    this.classList.add("videos__link--selected")
+    links.forEach(link => link.classList.remove("videos__link--selected"));
+    this.classList.add("videos__link--selected");
+    window.location.hash = this.dataset.src;
     video.src = `videos/${this.dataset.src}.mp4#t=0.01`;
 }
 
 function setVideoHeight() {
-    video.removeAttribute("height")
-    video.height = video.getBoundingClientRect().height
+    video.removeAttribute("height");
+    video.height = video.getBoundingClientRect().height;
 }
 
+// Grab DOM elements
 var video = document.getElementById("js-video");
 var links = document.querySelectorAll("[data-src]");
 
-links.forEach(link => link.addEventListener("click", changeVideo))
+// Add event listeners
+links.forEach(link => link.addEventListener("click", changeVideo));
+video.addEventListener("loadeddata", setVideoHeight);
+window.addEventListener("resize", setVideoHeight);
 
-video.addEventListener("loadeddata", setVideoHeight)
-window.addEventListener("resize", setVideoHeight)
+// Select video that is in window hash
+var selectedVideo = window.location.hash.substr(1);
+links.forEach(link => {
+    if (link.dataset.src == selectedVideo) {
+        link.click();
+        link.scrollIntoView()
+    }
+})
